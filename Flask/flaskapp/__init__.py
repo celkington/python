25f4f2c89471e58432d16572565ba04099b0dbc9
celkington <celkington2216@gmail.com>
2020-07-10 14:35:17 +0100

    User authentication update

--- Flask/flaskapp/__init__.py
+++ Flask/flaskapp/__init__.py
@@ -1,6 +1,8 @@
 # Imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 # Configuration
@@ -8,5 +10,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8a74d6b55af7824f00b861cf3b445fbe' # A secret key signs cookies when a user visits the site and therefore prevents cookie tampering
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from flaskapp import routes
--- Flask/flaskapp/forms.py
+++ Flask/flaskapp/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskapp.models import User
 import re
 
 def password_validate(form, field):
@@ -26,6 +27,15 @@ class RegistrationForm(FlaskForm):
                              validators= [DataRequired(), EqualTo('password', message= 'This does not match your previous password.')])
     submit = SubmitField('Register')
     
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
